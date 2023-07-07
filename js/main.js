8d9a68f120444f65facb774bca53e0ff7790c801
chianddo223 <110188103+chianddo223@users.noreply.github.com>
2023-07-07 21:14:46 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/* for(let i=0; i < 11; i++){
+    if(i%2===1){
+        continue;
+    }
+    console.log(`q1.${i}`);
+ }*/
+
+for(let i=0; i<11; i++){
+    if(i===9){
+        break;
+    }
+    console.log(`q2.${i}`);
+}
